c58e465a26500607f8bbef0e6d0b15496e8a64d8
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-06-07 19:54:04 -0500

    add modularity change function for moving isolated node into community, needs more testing

--- custom-leiden.py
+++ custom-leiden.py
@@ -3,11 +3,30 @@ import time
 import networkx as nx
 
 class CommunityData:
-    def __init__(self):
+    def __init__(self, G):
+        self.G = G
         self.nodes = {}
 
-    def add_node(self, node_id):
-        self.nodes[node_id] = True
+        self.sum_weights_in = 0
+        self.sum_weights_tot = 0
+
+    def add_node(self, node):
+        edges = self.G.edges(node, data=True)
+        for u, v, data in edges:
+            weight = data.get("weight", 1)
+
+            # If u or v are in self.nodes, then we do not need to increment self.sum_weights_tot
+            # as it will already be in there, given that this node was adjacent to the community.
+            # We do not want to double count it.
+
+            if u in self.nodes:
+                self.sum_weights_in += weight
+            elif v in self.nodes:
+                self.sum_weights_in += weight
+            else:
+                self.sum_weights_tot += weight
+        
+        self.nodes[node] = True
 
     def nodes(self):
         return self.nodes.keys()
@@ -20,6 +39,24 @@ def total_edge_weight(G):
 
     return tot
 
+# important: The "or" in here makes the assumption that this vertex is not yet
+# a part of the community (it would need to be "and" if already a member)
+def vertex_total_in_edge_weight(G, node, community):
+    edges = G.edges(node, data=True)
+
+    tot = 0
+    for u, v, data in edges:
+        weight = data.get("weight", 1)
+        data_u = G.nodes[u]
+        data_v = G.nodes[v]
+        comm_u = data_u["community"]
+        comm_v = data_v["community"]
+
+        if community == comm_u or community == comm_v:
+            tot += weight
+    
+    return tot
+
 def vertex_total_edge_weight(G, node):
     edges = G.edges(node, data=True)
 
@@ -28,7 +65,7 @@ def vertex_total_edge_weight(G, node):
         weight = data.get("weight", 1)
         tot += weight
     
-    return weight
+    return tot
 
 def modularity(G):
     m = total_edge_weight(G)
@@ -60,7 +97,30 @@ def modularity(G):
     return Q
 
 def isolated_move_modularity_change(G, community_graph, node, community):
-    return 0
+    # TODO: we should be passing this in, not recomputing it
+    m = total_edge_weight(G)
+
+    comm_data = community_graph.nodes[community]["community_data"]
+
+    sum_in = comm_data.sum_weights_in
+    sum_tot = comm_data.sum_weights_tot
+
+    print(sum_in, sum_tot)
+
+    # TODO: could be more efficient by counting these both at once
+    k_i_in = vertex_total_in_edge_weight(G, node, community)
+    k_i = vertex_total_edge_weight(G, node)
+
+    print(k_i_in, k_i)
+
+    new_comm_Q = ((sum_in + k_i_in) / (2 * m)) - ((sum_tot + k_i) / (2 * m))**2
+    prev_comm_Q = (sum_in / (2 * m)) - (sum_tot / (2 * m))**2 - (k_i / (2 * m))**2
+
+    print(new_comm_Q, prev_comm_Q)
+
+    delta_Q = new_comm_Q - prev_comm_Q
+
+    return delta_Q
 
 def leiden(G):
     pass
@@ -84,10 +144,10 @@ def main():
 
     community_graph = nx.Graph()
 
-    c0 = CommunityData()
+    c0 = CommunityData(G)
     c0.add_node(0)
 
-    c1 = CommunityData()
+    c1 = CommunityData(G)
     c1.add_node(1)
     c1.add_node(2)
     c1.add_node(3)
