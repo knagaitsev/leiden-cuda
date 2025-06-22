91b413dfa9a590b3f77a6ab9612162d056855966
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-06-22 17:36:13 -0500

    create new node_data_t and comm_data_t structs to pass aggregate count data to move_nodes_fast, calculate CPM correctly, do atomicAdd on community aggregate counts

--- csrc/leiden.cpp
+++ csrc/leiden.cpp
@@ -6,24 +6,50 @@
 #include <map>
 #include <algorithm>
 
+typedef struct node_data {
+    uint32_t community;
+    uint32_t agg_count;
+} node_data_t;
+
+typedef struct comm_data {
+    uint32_t agg_count;
+} comm_data_t;
+
 extern "C" void launch_add_kernel(float* a, float* b, float* c, int N);
 
-extern "C" void move_nodes_fast(uint32_t *offsets, uint32_t *indices, float *weights, uint32_t *communities, int vertex_count, int edge_count);
+extern "C" void move_nodes_fast(uint32_t *offsets, uint32_t *indices, float *weights, node_data_t *node_data, comm_data_t *comm_data, int vertex_count, int edge_count, int comm_count, float gamma);
 
 void leiden(std::vector<uint32_t> offsets, std::vector<uint32_t> indices, std::vector<float> weights) {
     int vertex_count = offsets.size() - 1;
     int edge_count = indices.size();
 
-    std::vector<uint32_t> communities;
+    std::vector<node_data_t> node_data;
+
+    std::vector<comm_data_t> comm_data;
+
     for (uint32_t i = 0; i < vertex_count; i++) {
-        communities.push_back(i);
+        node_data_t node = { .community = i, .agg_count = 1 };
+        node_data.push_back(node);
+
+        comm_data_t comm = { .agg_count = 1 };
+        comm_data.push_back(comm);
     }
+
+    int comm_count = vertex_count;
 
     float gamma = 0.05;
 
-    move_nodes_fast(offsets.data(), indices.data(), weights.data(), communities.data(), vertex_count, edge_count);
+    move_nodes_fast(offsets.data(), indices.data(), weights.data(), node_data.data(), comm_data.data(), vertex_count, edge_count, comm_count, gamma);
+
+    std::cout << "\n";
 
     for (int i = 0; i < vertex_count; i++) {
-        std::cout << "Community " << i << ": " << communities[i] << "\n";
+        std::cout << "Vertex " << i << " community: " << node_data[i].community << "\n";
+    }
+
+    std::cout << "\n";
+
+    for (int i = 0; i < comm_count; i++) {
+        std::cout << "Community " << i << " agg count: " << comm_data[i].agg_count << "\n";
     }
 }
--- csrc/leiden_kernel.cu
+++ csrc/leiden_kernel.cu
@@ -6,9 +6,9 @@ typedef struct node_data {
     uint32_t agg_count;
 } node_data_t;
 
-typedef struct community_data {
+typedef struct comm_data {
     uint32_t agg_count;
-} community_data_t;
+} comm_data_t;
 
 __global__ void add_kernel(float* a, float* b, float* c, int N) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -33,40 +33,55 @@ extern "C" void launch_add_kernel(float* a, float* b, float* c, int N) {
 
 // two approaches to doing move_nodes_fast: parallelizing at node level is below
 // - another option is parallelizing at edge level, letting each thread consider an edge
-__global__ void move_nodes_fast_kernel(uint32_t *offsets, uint32_t *indices, float *weights, uint32_t *communities, int vertex_count, int edge_count, float gamma) {
+__global__ void move_nodes_fast_kernel(uint32_t *offsets, uint32_t *indices, float *weights, node_data_t *node_data, comm_data_t *comm_data, int vertex_count, int edge_count, int comm_count, float gamma) {
     unsigned int node = threadIdx.x;
     
     // communities[threadIdx.x] = 1;
     uint32_t offset = offsets[node];
     uint32_t offsetNext = offsets[node + 1];
 
-    uint32_t curr_comm = communities[node];
+    uint32_t curr_comm = node_data[node].community;
 
     uint32_t best_comm = curr_comm;
     float best_delta = 0;
+
+    uint32_t node_edge_count = offsetNext - offset;
+
+    // aggregate count of nodes in old community (including current node)
+    int agg_count_old = comm_data[curr_comm].agg_count;
+
+    // aggregate count of current node
+    int node_agg_count = node_data[node].agg_count;
+
+    // total edge weight of incoming edges from old community
+    float k_vc_old = 0.0;
+    for (uint32_t i = offset; i < offsetNext; i++) {
+        if (node_data[i].community == curr_comm) {
+            k_vc_old += weights[i];
+        }
+    }
 
     for (uint32_t i = offset; i < offsetNext; i++) {
         uint32_t neighbor = indices[i];
         float weight = weights[i];
 
-        uint32_t neighbor_comm = communities[neighbor];
+        uint32_t neighbor_comm = node_data[neighbor].community;
 
         if (neighbor_comm == curr_comm || neighbor_comm == best_comm) {
             continue;
         }
 
+        // aggregate count of nodes in new community (excluding current node)
+        int agg_count_new = comm_data[neighbor_comm].agg_count;
+
         // total edge weight of incoming edges from new community
         float k_vc_new = 0.0;
-        // total edge weight of incoming edges from old community
-        float k_vc_old = 0.0;
-
-        // aggregate count of nodes in old community (including current node)
-        int agg_count_old = 0;
-        // aggregate count of nodes in new community (excluding current node)
-        int agg_count_new = 0;
-
-        // aggregate count of current node
-        int node_agg_count = 1;
+        // TODO: need to try moving this elsewhere
+        for (uint32_t i = offset; i < offsetNext; i++) {
+            if (node_data[i].community == neighbor_comm) {
+                k_vc_new += weights[i];
+            }
+        }
 
         float delta = (k_vc_new - gamma * (float)(node_agg_count * agg_count_new)) - (k_vc_old - gamma * (float)(node_agg_count * (agg_count_old - node_agg_count)));
 
@@ -77,7 +92,9 @@ __global__ void move_nodes_fast_kernel(uint32_t *offsets, uint32_t *indices, flo
     }
 
     if (best_comm != curr_comm) {
-        communities[node] = best_comm;
+        node_data[node].community = best_comm;
+        atomicAdd(&(comm_data[best_comm].agg_count), 1);
+        atomicAdd(&(comm_data[curr_comm].agg_count), -1);
     }
 }
 
@@ -98,7 +115,7 @@ T* copy_from_device(T* data_host, T* data_device, int len) {
     cudaMemcpy(data_host, data_device, size, cudaMemcpyDeviceToHost);
 }
 
-extern "C" void move_nodes_fast(uint32_t *offsets, uint32_t *indices, float *weights, uint32_t *communities, int vertex_count, int edge_count, float gamma) {
+extern "C" void move_nodes_fast(uint32_t *offsets, uint32_t *indices, float *weights, node_data_t *node_data, comm_data_t *comm_data, int vertex_count, int edge_count, int comm_count, float gamma) {
     // each thread of the cuda kernel considers one node and attempts to greedily increase the CPM
     // by moving it to the best neighboring community
 
@@ -109,22 +126,25 @@ extern "C" void move_nodes_fast(uint32_t *offsets, uint32_t *indices, float *wei
     uint32_t *offsets_device = allocate_and_copy_to_device(offsets, vertex_count + 1);
     uint32_t *indices_device = allocate_and_copy_to_device(indices, edge_count);
     float *weights_device = allocate_and_copy_to_device(weights, edge_count);
-    uint32_t *communities_device = allocate_and_copy_to_device(communities, vertex_count);
+    node_data_t *node_data_device = allocate_and_copy_to_device(node_data, vertex_count);
+    comm_data_t *comm_data_device = allocate_and_copy_to_device(comm_data, comm_count);
 
     dim3 dim_grid(1);
  	dim3 dim_block(vertex_count);
 
-	move_nodes_fast_kernel <<<dim_grid, dim_block>>> (offsets_device, indices_device, weights_device, communities_device, vertex_count, edge_count, gamma);
+	move_nodes_fast_kernel <<<dim_grid, dim_block>>> (offsets_device, indices_device, weights_device, node_data_device, comm_data_device, vertex_count, edge_count, comm_count, gamma);
 
     cudaDeviceSynchronize();
 
     copy_from_device(offsets, offsets_device, vertex_count + 1);
     copy_from_device(indices, indices_device, edge_count);
     copy_from_device(weights, weights_device, edge_count);
-    copy_from_device(communities, communities_device, vertex_count);
+    copy_from_device(node_data, node_data_device, vertex_count);
+    copy_from_device(comm_data, comm_data_device, comm_count);
 
     cudaFree(offsets_device);
     cudaFree(indices_device);
     cudaFree(weights_device);
-    cudaFree(communities_device);
+    cudaFree(node_data_device);
+    cudaFree(comm_data_device);
 }
