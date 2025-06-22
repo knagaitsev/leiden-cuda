82236ef1ffa351fda64b5b68893bc70833582176
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-06-22 14:05:46 -0500

    write skeleton of move_nodes_fast and corresponding kernel

--- csrc/leiden.cpp
+++ csrc/leiden.cpp
@@ -1,7 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include <vector>
+#include <map>
+#include <algorithm>
 
 extern "C" void launch_add_kernel(float* a, float* b, float* c, int N);
 
-void leiden(float *a, float *b, float *c, int N) {
-    launch_add_kernel(a, b, c, N);
+extern "C" void move_nodes_fast(uint32_t *offsets, uint32_t *indices, float *weights, uint32_t *communities, int vertex_count, int edge_count);
+
+void leiden(std::vector<uint32_t> offsets, std::vector<uint32_t> indices, std::vector<float> weights) {
+    int vertex_count = offsets.size() - 1;
+    int edge_count = indices.size();
+
+    std::vector<uint32_t> communities;
+    for (uint32_t i = 0; i < vertex_count; i++) {
+        communities[i] = i;
+    }
+
+    move_nodes_fast(offsets.data(), indices.data(), weights.data(), communities.data(), vertex_count, edge_count);
 }
--- csrc/leiden_kernel.cu
+++ csrc/leiden_kernel.cu
@@ -21,3 +21,16 @@ extern "C" void launch_add_kernel(float* a, float* b, float* c, int N) {
 
     cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
 }
+
+__global__ void move_nodes_fast_kernel() {
+
+}
+
+extern "C" void move_nodes_fast(uint32_t *offsets, uint32_t *indices, float *weights, uint32_t *communities, int vertex_count, int edge_count) {
+    // each thread of the cuda kernel considers one node and attempts to greedily increase the CPM
+    // by moving it to the best neighboring community
+
+    // the threads should be considering nodes in a semi-random order though.
+    // will it be better to give them an array of random indices to access,
+    // - or should we reorder the data structure to ensure a warp coalesces global memory accesses?
+}
