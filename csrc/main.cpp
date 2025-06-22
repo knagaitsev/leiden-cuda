1cc779c4c058708c9658a92f0c21448b855f076e
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2025-06-22 13:20:33 -0500

    complete to_csr implementation

--- csrc/main.cpp
+++ csrc/main.cpp
@@ -3,8 +3,65 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <map>
+#include <algorithm>
 
 void leiden(float *a, float *b, float *c, int N);
+
+typedef std::vector<std::pair<std::pair<uint32_t, uint32_t>, float>> edge_list_t;
+
+typedef std::pair<std::vector<uint32_t>, std::pair<std::vector<uint32_t>, std::vector<float>>> offsets_indices_weights_t;
+
+offsets_indices_weights_t to_csr(edge_list_t edge_list) {
+    std::map<uint32_t, std::vector<std::pair<uint32_t, float>>> vertex_map;
+
+    for (auto edge_data : edge_list) {
+        auto edge = edge_data.first;
+        auto weight = edge_data.second;
+
+        auto u = edge.first;
+        auto v = edge.second;
+
+        auto new_edge_to_u = std::make_pair(u, weight);
+        auto new_edge_to_v = std::make_pair(v, weight);
+
+        if (vertex_map.find(u) != vertex_map.end()) {
+            vertex_map[u].push_back(new_edge_to_v);
+        } else {
+            std::vector<std::pair<uint32_t, float>> neighbors;
+            neighbors.push_back(new_edge_to_v);
+            vertex_map[u] = neighbors;
+        }
+    }
+
+    std::vector<uint32_t> vertices;
+
+    std::vector<uint32_t> offsets;
+    std::vector<uint32_t> indices;
+    std::vector<float> weights;
+
+    auto curr_offset = 0;
+    for (auto kv : vertex_map) {
+        vertices.push_back(kv.first);
+        
+        // std::cout << "vertex: " << kv.first << "\n";
+
+        std::sort(kv.second.begin(), kv.second.end(), [](const auto& a, const auto& b) {
+            return a.first < b.first;
+        });
+
+        for (auto edge_data : kv.second) {
+            indices.push_back(edge_data.first);
+            weights.push_back(edge_data.second);
+        }
+
+        offsets.push_back(curr_offset);
+        curr_offset += kv.second.size();
+    }
+    offsets.push_back(curr_offset);
+
+    return std::make_pair(offsets, std::make_pair(indices, weights));
+}
 
 int main() {
     std::ifstream file("validation/clique_ring.txt");
@@ -14,7 +71,7 @@ int main() {
         return 1;
     }
 
-    std::vector<std::pair<uint32_t, uint32_t>> edge_list;
+    edge_list_t edge_list;
 
     std::string line;
     while (std::getline(file, line)) {
@@ -26,7 +83,8 @@ int main() {
         std::istringstream iss(line);
         uint32_t a, b;
         if (iss >> a >> b) {
-            edge_list.push_back(std::make_pair(a, b));
+            auto weight = 1.0;
+            edge_list.push_back(std::make_pair(std::make_pair(a, b), weight));
         }
     }
 
@@ -34,26 +92,36 @@ int main() {
 
     std::cout << "Edge list len: " << edge_list.size() << "\n";
 
-    int N = 1024;
-    float *a = new float[N];
-    float *b = new float[N];
-    float *c = new float[N];
+    auto offsets_indices_weights = to_csr(edge_list);
+    auto offsets = offsets_indices_weights.first;
+    auto indices_weights = offsets_indices_weights.second;
+    auto indices = indices_weights.first;
+    auto weights = indices_weights.second;
 
-    for (int i = 0; i < N; ++i) {
-        a[i] = 1.0f;
-        b[i] = 2.0f;
-    }
+    std::cout << "Offsets size: " << offsets.size() << "\n";
+    std::cout << "Indices size: " << indices.size() << "\n";
+    std::cout << "Weights size: " << weights.size() << "\n";
 
-    leiden(a, b, c, N);
+    // int N = 1024;
+    // float *a = new float[N];
+    // float *b = new float[N];
+    // float *c = new float[N];
 
-    std::cout << "Result: ";
-    for (int i = 0; i < 10; ++i) {
-        std::cout << c[i] << " ";
-    }
-    std::cout << "...\n";
+    // for (int i = 0; i < N; ++i) {
+    //     a[i] = 1.0f;
+    //     b[i] = 2.0f;
+    // }
 
-    delete[] a;
-    delete[] b;
-    delete[] c;
+    // leiden(a, b, c, N);
+
+    // std::cout << "Result: ";
+    // for (int i = 0; i < 10; ++i) {
+    //     std::cout << c[i] << " ";
+    // }
+    // std::cout << "...\n";
+
+    // delete[] a;
+    // delete[] b;
+    // delete[] c;
     return 0;
 }
